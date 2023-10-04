3d2edb888f796dcf7629e3f946c0cc36bbb779d0
slm <sloniklm@ya.ru>
2023-10-04 20:49:31 +0400

    Initial Commit

---
+++ Currency Widget/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Currency Widget
+//
+//  Created by macSlm on 04.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
